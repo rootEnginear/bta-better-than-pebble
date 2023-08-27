4f7a3777c0ea1b8f1e13e52e1434bbdc504b1c2c
rootEnginear <rootEnginear@users.noreply.github.com>
2023-08-27 21:52:01 +0700

    fix: adjust rock damage

--- src/main/java/rootenginear/betterthanpebble/item/Rock.java
+++ src/main/java/rootenginear/betterthanpebble/item/Rock.java
@@ -7,7 +7,7 @@ import net.minecraft.core.item.tool.ItemTool;
 
 public class Rock extends ItemTool {
     public Rock(String name, int id) {
-        super(name, id, 1, new ToolMaterial().setEfficiency(1.0F, 2.0F).setMiningLevel(0), BlockTags.MINEABLE_BY_AXE);
+        super(name, id, 3, new ToolMaterial().setEfficiency(1.0F, 2.0F).setMiningLevel(0), BlockTags.MINEABLE_BY_AXE);
     }
 
     public boolean canHarvestBlock(Block block) {
