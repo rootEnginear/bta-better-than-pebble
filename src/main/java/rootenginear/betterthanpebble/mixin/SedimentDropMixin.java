70b8f4166fc3df531d06442d6c826d6de1910ab9
rootEnginear <rootEnginear@users.noreply.github.com>
2023-08-28 00:51:23 +0700

    fix(refactor): sediment drops

--- src/main/java/rootenginear/betterthanpebble/mixin/SedimentDropMixin.java
+++ src/main/java/rootenginear/betterthanpebble/mixin/SedimentDropMixin.java
@@ -14,47 +14,53 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import rootenginear.betterthanpebble.utils.SedimentData;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @Mixin(value = {Block.class, BlockGravel.class, BlockGrass.class, BlockGrassScorched.class}, remap = false)
 public class SedimentDropMixin {
 
     @Unique
-    private final float gravelFlintChance = 39.8F;
+    private static final float gravelFlintChance = 39.8F;
     @Unique
-    private final float sandChance = 18.0F;
+    private static final float chanceByFlint = 1.0F / gravelFlintChance / 10.0F;
+
     @Unique
-    private final float gravelChance = 20.0F;
+    private static final float sandChance = 18.0F * chanceByFlint;
     @Unique
-    private final float richDirtChance = 3.0F;
+    private static final float gravelChance = 20.0F * chanceByFlint;
     @Unique
-    private final float dirtChance = 60.2F;
+    private static final float scorchedDirtChance = 3.0F * chanceByFlint;
     @Unique
-    private final float chanceByFlint = 1.0F / gravelFlintChance / 10.0F;
+    private static final float dirtChance = 60.2F * chanceByFlint;
+
+    @Unique
+    private static final Map<Integer, SedimentData> sedimentLookup = new HashMap<>();
+
+    static {
+        sedimentLookup.put(Block.sand.id, new SedimentData(sandChance, false, 4));
+        sedimentLookup.put(Block.gravel.id, new SedimentData(gravelChance, true, 5));
+        sedimentLookup.put(Block.dirtScorched.id, new SedimentData(scorchedDirtChance, false, 4));
+        sedimentLookup.put(Block.dirtScorchedRich.id, new SedimentData(scorchedDirtChance, false, 4));
+        sedimentLookup.put(Block.grassScorched.id, new SedimentData(scorchedDirtChance, true, 4));
+        sedimentLookup.put(Block.dirt.id, new SedimentData(dirtChance, false, 2));
+        sedimentLookup.put(Block.grassRetro.id, new SedimentData(dirtChance, false, 2));
+        sedimentLookup.put(Block.grass.id, new SedimentData(dirtChance, true, 2));
+    }
 
     @Inject(method = "getBreakResult", at = @At("HEAD"), cancellable = true)
     private void sedimentBreakResult(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity, CallbackInfoReturnable<ItemStack[]> cir) {
         Block self = (Block) (Object) this;
         float rand = new Random().nextFloat();
 
-        if (self.id == Block.sand.id && rand < sandChance * chanceByFlint) {
-            cir.setReturnValue(new ItemStack[]{new ItemStack(Item.ammoPebble, new Random().nextInt(4) + 1)});
-            return;
-        }
-
-        if (self.id == Block.gravel.id && rand < gravelChance * chanceByFlint) {
-            cir.setReturnValue(new ItemStack[]{new ItemStack(Item.ammoPebble, new Random().nextInt(5) + 1)});
-            return;
-        }
-
-        if ((self.id == Block.dirtScorched.id || self.id == Block.dirtScorchedRich.id || self.id == Block.grassScorched.id) && rand < richDirtChance * chanceByFlint) {
-            cir.setReturnValue(new ItemStack[]{new ItemStack(Item.ammoPebble, new Random().nextInt(4) + 1)});
-            return;
-        }
-
-        if ((self.id == Block.dirt.id || self.id == Block.grass.id || self.id == Block.grassRetro.id) && rand < dirtChance * chanceByFlint) {
-            cir.setReturnValue(new ItemStack[]{new ItemStack(Item.ammoPebble, new Random().nextInt(2) + 1)});
+        SedimentData data = sedimentLookup.get(self.id);
+        if (data != null && rand < data.chance) {
+            if (data.pickSilkCheck && (dropCause == EnumDropCause.PICK_BLOCK || dropCause == EnumDropCause.SILK_TOUCH))
+                return;
+            cir.setReturnValue(new ItemStack[]{new ItemStack(Item.ammoPebble, new Random().nextInt(data.maxDrop) + 1)});
         }
     }
 }
---
+++ src/main/java/rootenginear/betterthanpebble/utils/SedimentData.java
@@ -0,0 +1,13 @@
+package rootenginear.betterthanpebble.utils;
+
+public class SedimentData {
+    public float chance;
+    public boolean pickSilkCheck;
+    public int maxDrop;
+
+    public SedimentData(float chance, boolean pickSilkCheck, int maxDrop) {
+        this.chance = chance;
+        this.pickSilkCheck = pickSilkCheck;
+        this.maxDrop = maxDrop;
+    }
+}
