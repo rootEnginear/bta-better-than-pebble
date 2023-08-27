bd18c53c44c321472e8bd025d416cb927cff6811
rootEnginear <rootEnginear@users.noreply.github.com>
2023-08-27 17:44:30 +0700

    feat: add rock

--- src/main/java/rootenginear/betterthanpebble/BetterThanPebble.java
+++ src/main/java/rootenginear/betterthanpebble/BetterThanPebble.java
@@ -5,16 +5,21 @@ import net.minecraft.core.block.Block;
 import net.minecraft.core.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.ItemHelper;
 import turniplabs.halplibe.helper.RecipeHelper;
 
 public class BetterThanPebble implements ModInitializer {
-    public static final String MOD_ID = "better-than-pebble";
+    public static final String MOD_ID = "betterthanpebble";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     static {
         // Prevent Java compiler to remove unused import
-        Block $ = Block.blockOlivine;
+        Block $1 = Block.blockOlivine;
+        Item $2 = Item.olivine;
     }
+
+    public static final int ITEM_ID = 31415;
+    public static final Item rockItem = ItemHelper.createItem(MOD_ID, new Item(ITEM_ID), "rock", "rock.png");
 
     @Override
     public void onInitialize() {
@@ -24,6 +29,21 @@ public class BetterThanPebble implements ModInitializer {
         RecipeHelper.Crafting.removeRecipe(Item.toolPickaxeWood);
         RecipeHelper.Crafting.removeRecipe(Item.toolShovelWood);
         RecipeHelper.Crafting.removeRecipe(Item.toolSwordWood);
+
+        // Remove Cobble Item Recipe
+        RecipeHelper.Crafting.removeRecipe(Block.cobbleStone);
+
+        // Add Rock Recipes
+        RecipeHelper.Crafting.createRecipe(rockItem, 1, new Object[]{
+                "AA",
+                "AA",
+                'A', Item.ammoPebble
+        });
+        RecipeHelper.Crafting.createRecipe(Block.cobbleStone, 1, new Object[]{
+                "AA",
+                "AA",
+                'A', rockItem
+        });
 
         LOGGER.info("Better Than Pebble initialized.");
     }
